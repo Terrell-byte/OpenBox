8f5e9a460328d4fd526f75f391ed7e37ea43a9d9
Terrell-byte <danieltsutton@outlook.com>
2024-07-14 18:30:30 +0200

    feat: add fn for get_user trait

--- openbox/src/Domain/entities/UserEntity.rs
+++ openbox/src/Domain/entities/UserEntity.rs
@@ -1,5 +1,7 @@
-public struct User {
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
     pub public_key: String,
     pub created_at: chrono::DateTime<chrono::Utc>,
-    
 }
--- openbox/src/Infrastructure/Repositories/UserRepository.rs
+++ openbox/src/Infrastructure/Repositories/UserRepository.rs
@@ -12,11 +12,10 @@ impl UserRepository {
     pub async fn new(db: Surreal<Client>) -> Self {
         Self { db }
     }
-
-    pub async fn create_user(&self, user: &User) -> Result<(), Box<dyn std::error::Error>> {
-        self.db
-            .insert("users", user)
+    pub async fn get_user(&self, public_key: &str) -> Result<User, Box<dyn std::error::Error>> {
+        let user: Option<User> = self.db
+            .select(("users", public_key))
             .await?;
-        Ok(())
+        user.ok_or_else(|| Box::new(std::io::Error::new(std::io::ErrorKind::NotFound, "!USER NOT FOUND!")))     
     }
 }
